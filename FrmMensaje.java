0d17795b60019513965e65a7c54b9baaab3c529a
lduran <lduran@github.com>
2013-09-07 21:52:43 -0300

    Realice cambios en frmmensaje desde master

--- FrmMensaje.java
+++ FrmMensaje.java
@@ -13,7 +13,7 @@ public class FrmMensaje extends Activity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.frmmensaje);
-		// Comentario desde master cargo el text view
+		// Comentario desde master cargo el text view agrego comentario desde master
 		TextView txtMensaje = (TextView)findViewById(R.id.txtMensaje);
 		Bundle bundle = getIntent().getExtras();
 		txtMensaje.setText("Hola"+ bundle.getString("NOMBRE"));
