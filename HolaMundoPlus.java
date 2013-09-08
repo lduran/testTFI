b02df6058ff8431d7d63f8c4e02a0ed6895fc08c
lduran <lduran@github.com>
2013-09-07 21:35:31 -0300

    Nuevo comentario sobre HolaMundoPlus

--- HolaMundoPlus.java
+++ HolaMundoPlus.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-
+//activity principal
 public class HolaMundoPlus extends Activity {
 
     @Override
